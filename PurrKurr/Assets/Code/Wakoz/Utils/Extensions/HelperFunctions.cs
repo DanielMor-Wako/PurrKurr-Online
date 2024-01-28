754c5d1ff0db36e08173ab18169b0d7e80c5e1aa
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2024-01-29 00:24:06 +0200

    helper functions class

---
+++ PurrKurr/Assets/Code/Wakoz/Utils/Extensions/HelperFunctions.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Code.Wakoz.Utils.Extensions {
+    public static class HelperFunctions {
+
+        public static string GetString(this byte[] self) {
+            return Encoding.UTF8.GetString(self);
+        }
+
+        public static string ToBase64(this string self) {
+            var plainTextBytes = Encoding.UTF8.GetBytes(self);
+
+            return Convert.ToBase64String(plainTextBytes);
+        }
+
+        public static string FromBase64(this string self) {
+            var base64EncodedBytes = Convert.FromBase64String(self);
+
+            return Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+
+        public static string ToString(this object self, Color color) {
+            return "<color=#" + ColorUtility.ToHtmlStringRGBA(color) + ">" + self + "</color>";
+        }
+
+        public static bool AreDictionariesEqual<T1, T2>(Dictionary<T1, T2> lDictionary, Dictionary<T1, T2> rDictionary)
+            where T2 : IEquatable<T2> {
+            if (rDictionary == null ||
+                lDictionary == null) {
+                return false;
+            }
+
+            foreach (var pair in rDictionary) {
+                if (!lDictionary.ContainsKey(pair.Key) ||
+                    !lDictionary[pair.Key].Equals(pair.Value)) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool IsNullOrEmpty<T>(this ICollection<T> collection) {
+            return collection == null || collection.Count == 0;
+        }
+
+        public static bool IsNullOrEmpty<T>(this IList<T> collection) {
+            return collection == null || collection.Count == 0;
+        }
+
+        public static void SetLeft(this RectTransform rt, float left) {
+            rt.offsetMin = new Vector2(left, rt.offsetMin.y);
+        }
+
+        public static void SetRight(this RectTransform rt, float right) {
+            rt.offsetMax = new Vector2(-right, rt.offsetMax.y);
+        }
+
+        public static void SetTop(this RectTransform rt, float top) {
+            rt.offsetMax = new Vector2(rt.offsetMax.x, -top);
+        }
+
+        public static void SetBottom(this RectTransform rt, float bottom) {
+            rt.offsetMin = new Vector2(rt.offsetMin.x, bottom);
+        }
+
+        public static void Shuffle(this IList list) {
+            var rand = new System.Random();
+
+            for (int i = list.Count - 1; i > 0; --i) {
+                var j = rand.Next(i + 1);
+                (list[i], list[j]) = (list[j], list[i]);
+            }
+        }
+
+        public static T Random<T>(this IList<T> list) {
+            var rand = new System.Random();
+
+            return list[rand.Next(list.Count)];
+        }
+
+        public static bool SameAs(this string self, string other) {
+            return self.Equals(other, StringComparison.OrdinalIgnoreCase);
+        }
+
+    }
+    
+}
