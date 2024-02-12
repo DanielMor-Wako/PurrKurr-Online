15d73a0ac562985deea802fe836abfc14f8a9134
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2024-02-12 19:57:31 +0200

    objectPool implementation

---
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/Screens/InteractableObjectsPool/InteractablesController.cs
@@ -0,0 +1,115 @@
+using Code.Wakoz.PurrKurr.DataClasses.Characters;
+using Code.Wakoz.PurrKurr.DataClasses.GameCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Pool;
+
+namespace Code.Wakoz.PurrKurr.Screens.InteractableObjectsPool {
+
+    [DefaultExecutionOrder(12)]
+    public sealed class InteractablesController : SingleController {
+
+        public List<Character2DController> _heroes;
+        public List<Projectile2D> _projectiles;
+
+        private Dictionary<System.Type, object> objectPools = new Dictionary<System.Type, object>();
+
+        protected override void Clean() { }
+
+        protected override Task Initialize() {
+
+            return Task.CompletedTask;
+        }
+
+        public void CreateObjectPool<T>(T prefab, int initialSize, int maxCapacity, string containerName) where T : MonoBehaviour, IInteractableBody {
+
+            if (!objectPools.ContainsKey(typeof(T))) {
+                var container = new GameObject($"{containerName}");
+                container.transform.SetParent(transform, false);
+                InteractableObjectPool<T> pool = new InteractableObjectPool<T>(prefab, container.transform, initialSize, maxCapacity);
+                objectPools.Add(typeof(T), pool);
+            }
+        }
+
+        public T GetInstance<T>() where T : MonoBehaviour, IInteractableBody {
+
+            if (objectPools.ContainsKey(typeof(T))) {
+                InteractableObjectPool<T> pool = (InteractableObjectPool<T>)objectPools[typeof(T)];
+                return pool.GetObjectFromPool();
+            } else {
+                Debug.LogError("Object pool for type " + typeof(T) + " does not exist.");
+                return null;
+            }
+        }
+
+        public List<T> GetAllInstances<T>() where T : MonoBehaviour, IInteractableBody {
+            if (objectPools.ContainsKey(typeof(T))) {
+                InteractableObjectPool<T> pool = (InteractableObjectPool<T>)objectPools[typeof(T)];
+                return pool.GetAllObjectsFromPool();
+            } else {
+                Debug.LogError("Object pool for type " + typeof(T) + " does not exist.");
+                return new List<T>();
+            }
+        }
+
+        public Task ReleaseInstance<T>(T instance) where T : MonoBehaviour, IInteractableBody {
+
+            if (objectPools.ContainsKey(typeof(T))) {
+                InteractableObjectPool<T> pool = (InteractableObjectPool<T>)objectPools[typeof(T)];
+                pool.ReleaseObjectToPool(instance);
+            } else {
+                Debug.LogError("Object pool for type " + typeof(T) + " does not the instance for release");
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+
+    public class InteractableObjectPool<T> where T : MonoBehaviour, IInteractableBody {
+
+        private ObjectPool<T> objectPool;
+        private readonly List<T> _instances = new();
+        private Transform _container;
+        private int _initialSize;
+        private int _maxCapacity;
+
+        public InteractableObjectPool(T prefab, Transform container, int initialSize, int maxCapacity) {
+            
+            _container = container;
+            _initialSize = initialSize;
+            _maxCapacity = maxCapacity;
+
+            objectPool = new ObjectPool<T>(() => Object.Instantiate(prefab, container));
+        }
+
+        public T GetObjectFromPool() {
+
+            if (objectPool == null) {
+                Debug.LogError($"ObjectPool {typeof(T).Name} was not initialized");
+                return null;
+            }
+
+            if (objectPool.CountAll <= _maxCapacity || objectPool.CountInactive > 0) {
+                T newObj = objectPool.Get();
+                newObj.transform.SetParent(_container, false);
+                newObj.gameObject.SetActive(true);
+                _instances.Add(newObj);
+                return newObj;
+            }
+
+            return null;
+        }
+
+        public List<T> GetAllObjectsFromPool() => _instances;
+
+        public void ReleaseObjectToPool(T instance) {
+            if (objectPool != null && instance != null && _instances.Contains(instance)) {
+                instance.gameObject.SetActive(false);
+                objectPool.Release(instance);
+            } else {
+                Debug.LogWarning("Unable to release the object to the pool. Please ensure the object and the object pool are valid.");
+            }
+        }
+    }
+}
