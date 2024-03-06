8662e66cba798a1b61ad5571fb6df8ed3e40398b
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2024-03-06 15:12:40 +0200

    Detection Trigger zones as base class

---
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/DataClasses/GameCore/Detection/DetectionZoneTrigger.cs
@@ -0,0 +1,54 @@
+﻿using Code.Wakoz.PurrKurr.Popups.OverlayWindow;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Code.Wakoz.PurrKurr.DataClasses.GameCore.Detection {
+
+    [DefaultExecutionOrder(15)]
+    public class DetectionZoneTrigger : Controller {
+
+        public event Action<Collider2D> OnColliderEntered;
+        public event Action<Collider2D> OnColliderExited;
+
+        public List<OverlayWindowData> overlayWindowData;
+
+        [SerializeField] private DetectionZone zone;
+
+        public List<OverlayWindowData> GetWindowData() => overlayWindowData;
+
+        protected override void Clean() {
+
+            if (zone == null) {
+                return;
+            }
+            
+            zone.OnColliderEntered -= handleColliderEntered;
+            zone.OnColliderExited -= handleColliderExited;
+        }
+
+        protected override Task Initialize() {
+
+            Init();
+
+            return Task.CompletedTask;
+        }
+
+        private void Init() {
+
+            if (zone == null) {
+                return;
+            }
+
+            zone.OnColliderEntered += handleColliderEntered;
+            zone.OnColliderExited += handleColliderExited;
+        }
+
+        private void handleColliderExited(Collider2D triggeredCollider) => OnColliderExited?.Invoke(triggeredCollider);
+
+        private void handleColliderEntered(Collider2D triggeredCollider) => OnColliderEntered?.Invoke(triggeredCollider);
+
+    }
+
+}
