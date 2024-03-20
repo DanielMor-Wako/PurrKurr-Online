537a52e296517a6149df250175b2ae1a748e9d67
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2024-03-20 19:01:15 +0200

    timeline event

---
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/Screens/Gameplay Controller/TimelineEvent.cs
@@ -0,0 +1,31 @@
+﻿using Code.Wakoz.PurrKurr.DataClasses.Characters;
+using Code.Wakoz.PurrKurr.DataClasses.GameCore;
+using Code.Wakoz.PurrKurr.DataClasses.ScriptableObjectData;
+using UnityEngine;
+using static Code.Wakoz.PurrKurr.DataClasses.Enums.Definitions;
+
+namespace Code.Wakoz.PurrKurr.Screens.Gameplay_Controller {
+    public class TimelineEvent {
+
+        public Character2DController Attacker { get; private set; }
+        public IInteractableBody[] Targets { get; private set; }
+        public float StartTime { get; private set; }
+        public float DurationInMilliseconds { get; private set; }
+        public Vector2 InteractionPosition { get; private set; }
+        public AttackData AttackData { get; private set; }
+        public AttackAbility InteractionType { get; private set; }
+        public AttackBaseStats AttackProperties { get; private set; }
+
+        public TimelineEvent(Character2DController attacker, IInteractableBody[] targets, float startTime, float duration, Vector2 interactionPosition, AttackData attackData, AttackAbility interactionType, AttackBaseStats attackProperties) {
+            
+            Attacker = attacker;
+            Targets = targets;
+            StartTime = startTime;
+            DurationInMilliseconds = duration;
+            InteractionPosition = interactionPosition;
+            AttackData = attackData;
+            InteractionType = interactionType;
+            AttackProperties = attackProperties;
+        }
+    }
+}
