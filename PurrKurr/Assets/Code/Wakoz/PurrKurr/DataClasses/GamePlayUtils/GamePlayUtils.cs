18c2fe20c1f4e470c08809f58363fec96443f49a
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2024-03-26 15:01:37 +0200

    Update GamePlayUtils.cs

--- PurrKurr/Assets/Code/Wakoz/PurrKurr/DataClasses/GamePlayUtils/GamePlayUtils.cs
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/DataClasses/GamePlayUtils/GamePlayUtils.cs
@@ -16,8 +16,8 @@ namespace Code.Wakoz.PurrKurr.DataClasses.GamePlayUtils {
         [Header("Slomo Settings")]
         [SerializeField][Range(0.1f, 1f)] public float _defaultTimeScale = 1f;
         [SerializeField] private bool _affectTimeScale = true;
-        [SerializeField][Min(0)] public float _transitionDuration = 0.5f;
-        [SerializeField][Range(0.1f, 1f)] public float _slomoTimeScale = 0.5f;
+        [SerializeField][Min(0)] public float _transitionDuration = 0.2f;
+        [SerializeField][Range(0.075f, 1f)] public float _slomoTimeScale = 0.5f;
 
         private float _timeScaleTarget;
         private Coroutine _timeScaleTransitionCoroutine;
