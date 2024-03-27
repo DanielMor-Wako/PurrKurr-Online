f24a79db27d9388cf9fd6cc6412b996e162da267
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2024-03-27 22:07:30 +0200

    actions timing asset for duration and cooldown

--- PurrKurr/Assets/Code/Wakoz/PurrKurr/DataClasses/ScriptableObjectData/AttackBaseStats.cs
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/DataClasses/ScriptableObjectData/AttackBaseStats.cs
@@ -10,10 +10,16 @@ namespace Code.Wakoz.PurrKurr.DataClasses.ScriptableObjectData {
         public string _elementName;
         
         [Tooltip("The Attack Ability")]
-        [SerializeField] Definitions.AttackAbility _attackAbility;
-        
+        [SerializeField] private Definitions.AttackAbility _attackAbility;
+
+        [Tooltip("The duration of the delay before the action start, 0.15f is the fastest duration that a character can move so its the min action duration")]
+        [SerializeField][Min(0.15f)] private float _actionDuration;
+
+        [Tooltip("The duration of the cooldown after the action")]
+        [SerializeField][Min(0)] private float _actionCooldownDuration;
+
         [Tooltip("The attack properties when activated")]
-        [SerializeField]private List<Definitions.AttackProperty> _properties;
+        [SerializeField] private List<Definitions.AttackProperty> _properties;
 
         [Tooltip("The character conditional state for perform the action")]
         [SerializeField] private List<Definitions.ObjectState> _characterStateCondition;
@@ -24,7 +30,11 @@ namespace Code.Wakoz.PurrKurr.DataClasses.ScriptableObjectData {
 
         // todo: maybe add the projectile attack to the attack list?
         public Definitions.AttackAbility Ability => _attackAbility;
-        
+
+        public float ActionDuration => _actionDuration;
+               
+        public float ActionCooldownDuration => _actionCooldownDuration;
+
         public List<Definitions.AttackProperty> Properties => _properties;
         
         public List<Definitions.ObjectState> CharacterStateConditions => _characterStateCondition;
