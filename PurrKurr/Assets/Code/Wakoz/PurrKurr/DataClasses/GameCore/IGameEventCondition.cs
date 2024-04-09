4579ca9a621af7826770d7653b8b5fa437076979
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2024-04-09 19:07:23 +0300

    Game event interface for managing game events based on character state, stats etc

---
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/DataClasses/GameCore/IGameEventCondition.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Code.Wakoz.PurrKurr.DataClasses.GameCore {
+
+    public interface IGameEventCondition {
+        public void StartCheckingCondition(IInteractableBody targetBody, Action onConditionMetAction);
+        public void EndCheckingCondition();
+    }
+}
