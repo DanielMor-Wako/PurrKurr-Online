90a7d41cad7eabcfada138728ce5210b007839a7
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2024-04-09 19:08:22 +0300

    implementation of game event condition

---
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/DataClasses/GameCore/OverlayWindowTrigger/OverlayWindowConditionalCharacterState.cs
@@ -0,0 +1,21 @@
+﻿using Code.Wakoz.PurrKurr.DataClasses.Enums;
+
+namespace Code.Wakoz.PurrKurr.DataClasses.GameCore.OverlayWindowTrigger {
+    public class OverlayWindowConditionalCharacterState : OverlayWindowCondition<Definitions.ObjectState> {
+
+        /// <summary>
+        /// Holds character state condition to match, 
+        /// if the condition is met, the window passes to the next page or quit if its the last page
+        /// </summary>
+        
+        private Definitions.ObjectState _currentState;
+
+        public override bool HasMetCondition() {
+            return _currentState == ValueToMatch;
+        }
+
+        public override void UpdateCurrentValue() {
+            _currentState = TargetBody.GetCurrentState();
+        }
+    }
+}
