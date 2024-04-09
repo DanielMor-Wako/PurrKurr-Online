53227e4268237276a762a393f9edbd94ea23b44e
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2024-04-09 19:08:47 +0300

    code for ref individual conditions in the scene

---
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/DataClasses/GameCore/OverlayWindowTrigger/OverlayWindowConditions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Code.Wakoz.PurrKurr.DataClasses.GameCore.OverlayWindowTrigger {
+    public class OverlayWindowConditions : MonoBehaviour {
+
+        [SerializeField] private List<OverlayWindowConditionData> _conditions;
+
+        private Dictionary<int, IGameEventCondition> _conditionsPerPage;
+
+        public IGameEventCondition GetEventCondition(int pageIndex) {
+            return _conditionsPerPage.ContainsKey(pageIndex) ? _conditionsPerPage[pageIndex] : null;
+        }
+
+        private void Awake() {
+
+            InitConditions();
+        }
+
+        private void InitConditions() {
+            
+            if (_conditions == null || _conditions.Count == 0) {
+                return;
+            }
+
+            _conditionsPerPage = new();
+            for (int i = 0; i < _conditions.Count; i++) {
+                var eventCondition = _conditions[i].ConditionRef.GetComponent<IGameEventCondition>();
+                if (eventCondition == null) {
+                    Debug.LogWarning("Component ref Must be of type IGameEventCondition");
+                    continue;
+                }
+                _conditionsPerPage.Add(i, eventCondition);
+            }
+        }
+    }
+
+    [Serializable]
+    public class OverlayWindowConditionData {
+        [SerializeField] public int PageIndex;
+        [SerializeField] public Component ConditionRef;
+    }
+}
