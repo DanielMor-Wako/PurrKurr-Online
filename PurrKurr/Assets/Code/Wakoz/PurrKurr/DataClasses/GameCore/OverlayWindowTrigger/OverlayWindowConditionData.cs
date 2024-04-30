8c769c17781ef4b8c0442ce0899c1e93ff96f25b
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2024-04-30 21:03:29 +0300

    Create OverlayWindowConditionData.cs

---
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/DataClasses/GameCore/OverlayWindowTrigger/OverlayWindowConditionData.cs
@@ -0,0 +1,16 @@
+﻿using Code.Wakoz.PurrKurr.UI.Instructions;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Code.Wakoz.PurrKurr.DataClasses.GameCore.OverlayWindowTrigger {
+    [Serializable]
+    public class OverlayWindowConditionData {
+        [Tooltip("Page index to apply the modifiers")]
+        [SerializeField] public int PageIndex;
+        [Tooltip("Conditional component to check for and then perform an Action")]
+        [SerializeField] public Component ConditionRef;
+        [Tooltip("Animated Action to display over the UI-Inputs")]
+        [SerializeField] public List<OverlayWindowAnimationData> AnimatedAction;
+    }
+}
