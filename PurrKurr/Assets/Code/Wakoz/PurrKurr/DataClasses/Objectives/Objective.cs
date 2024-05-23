596c470f4a455aabcc3e770203847352bf9f9356
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2024-05-23 14:13:16 +0300

    generic objective class

---
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/DataClasses/Objectives/Objective.cs
@@ -0,0 +1,33 @@
+﻿using Code.Wakoz.PurrKurr.DataClasses.ScriptableObjectData;
+
+namespace Code.Wakoz.PurrKurr.DataClasses.Objectives {
+    public abstract class Objective<T> : IObjective where T : ObjectiveDataSO {
+
+        protected T _data;
+
+        public void Initialize(ObjectiveDataSO data) {
+
+            if (data is not T) {
+                UnityEngine.Debug.LogError("Invalid objective data for " + typeof(T).Name);
+                return;
+            }
+
+            _data = (T)data;
+            InitializeSpecific();
+        }
+
+        public abstract bool IsComplete();
+
+        public abstract void UpdateProgress(int amount);
+
+        public abstract string GetObjectiveDescription();
+
+        protected abstract void InitializeSpecific();
+
+        public abstract void Finish();
+
+        public string GetUniqueId() => _data.uniqueId;
+
+        public string GetObjectType() => _data.targetObjectType;
+    }
+}
