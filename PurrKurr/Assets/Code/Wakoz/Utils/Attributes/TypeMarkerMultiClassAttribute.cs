a09dad94608677ffe07986872b98a70c78d1f7f2
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2024-05-23 14:11:24 +0300

    Custome attribute class to map dataclass to a class (objetiveData to oBjectiveClass)

---
+++ PurrKurr/Assets/Code/Wakoz/Utils/Attributes/TypeMarkerMultiClassAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Code.Wakoz.Utils.Attributes {
+
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class TypeMarkerMultiClassAttribute : Attribute {
+
+        public Type type { get; }
+
+        public TypeMarkerMultiClassAttribute(Type objectiveType) {
+            type = objectiveType;
+        }
+    }
+}
