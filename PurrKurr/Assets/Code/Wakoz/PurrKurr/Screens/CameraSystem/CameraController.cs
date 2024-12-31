db22b60e157f9181b45be5e45a213f1009007992
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2024-12-31 14:36:00 +0200

    Camera System Folder and Camera Controller

---
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/Screens/CameraSystem/CameraController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System;
+using Code.Wakoz.PurrKurr.Screens.Gameplay_Controller;
+
+namespace Code.Wakoz.PurrKurr.Screens.CameraComponents
+{
+    [RequireComponent(typeof(Camera))]
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private Transform _cameraFocus;
+
+        private CameraHandler _cameraHandler;
+        private ICameraCharacterMediator _characterMediator;
+
+        public ICameraCharacterMediator CharacterMediator => _characterMediator;
+
+        public void BindCharacter(Transform character, GameplayController gameEvents)
+        {
+            _cameraHandler ??= new CameraHandler(this);
+
+            _characterMediator?.UnbindEvents();
+            
+            _characterMediator = new CameraCharacterMediator(_cameraHandler, gameEvents, _cameraFocus, character);
+            _characterMediator.BindEvents();
+        }
+
+        public void AddCamera(CameraData data, Func<Action> processActionCallback = null)
+        {
+            _cameraHandler ??= new CameraHandler(this);
+            _cameraHandler.EnqueueCamera(data, processActionCallback);
+        }
+
+        private void Start()
+        {
+            _cameraHandler ??= new CameraHandler(this);
+        }
+
+        private void Update()
+        {
+            _cameraHandler.UpdateCurrent();
+        }
+
+        private void OnDestroy()
+        {
+            _characterMediator?.UnbindEvents();
+        }
+    }
+}
