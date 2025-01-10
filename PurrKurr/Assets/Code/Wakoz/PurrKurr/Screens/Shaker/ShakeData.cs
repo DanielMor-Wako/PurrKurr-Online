4bac27078f1d8c2e887bcabfa71eefd04d542612
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2025-01-10 16:31:32 +0200

    Shake Data , interface and styles by enum

---
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/Screens/Shaker/ShakeData.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Code.Wakoz.PurrKurr.Screens.Shaker
+{
+    [System.Serializable]
+    public class ShakeData
+    {
+        public ShakeStyle ShakeStyle = ShakeStyle.Random;
+        public float Duration = 0.2f;
+        public float Intensity = 0.5f;
+        public AnimationCurve IntensityCurve = AnimationCurve.Linear(0, 1, 1, 0);
+        public Vector3 DefaultOffset = Vector3.zero;
+
+        public ShakeData(ShakeStyle shakeStyle) 
+        { 
+            ShakeStyle = shakeStyle; 
+        }
+
+        public ShakeData(ShakeStyle shakeStyle, float duration, float intensity = 0.5f, AnimationCurve intensityCurve = null, Vector3 defaultOffset = default)
+        {
+            ShakeStyle = shakeStyle;
+            Duration = duration;
+            Intensity = intensity;
+            IntensityCurve = intensityCurve ?? AnimationCurve.Linear(0, 1, 1, 0);
+            DefaultOffset = defaultOffset;
+        }
+    }
+    
+}
---
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/Screens/Shaker/ShakeStyle.cs
@@ -0,0 +1,13 @@
+﻿namespace Code.Wakoz.PurrKurr.Screens.Shaker
+{
+    public enum ShakeStyle
+    {
+        Random = 0,
+        Horizontal = 1,
+        Vertical = 2,
+        Circular = 3,
+        HorizontalCircular = 4,
+        VerticalCircular = 5,
+    }
+    
+}
