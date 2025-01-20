adf72398b9725429683314cf1702a4436d701334
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2025-01-20 23:30:13 +0200

    refactor

--- PurrKurr/Assets/Code/Wakoz/PurrKurr/DataClasses/GameCore/IInteractableBody.cs
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/DataClasses/GameCore/IInteractableBody.cs
@@ -1,27 +1,26 @@
-﻿using Code.Wakoz.PurrKurr.DataClasses.Characters;
-using Code.Wakoz.PurrKurr.DataClasses.Enums;
+﻿using Code.Wakoz.PurrKurr.DataClasses.Enums;
 using UnityEngine;
 
 namespace Code.Wakoz.PurrKurr.DataClasses.GameCore {
 
     public interface IInteractableBody {
 
-        public abstract Collider2D GetCollider();
-        public abstract Transform GetTransform();
-        public abstract Transform GetCharacterRigTransform();
-        public abstract Definitions.ObjectState GetCurrentState();
-        public abstract Vector3 GetCenterPosition();
-        public abstract Vector2 GetVelocity();
-        public abstract float GetHpPercent();
-        public abstract int DealDamage(int damage);
-        public abstract void ApplyForce(Vector2 forceDir);
-        public abstract void SetTargetPosition(Vector2 position, float percentToPerform = 1);
+        public Collider2D GetCollider();
+        public Transform GetTransform();
+        public Transform GetCharacterRigTransform();
+        public Definitions.ObjectState GetCurrentState();
+        public Vector3 GetCenterPosition();
+        public Vector2 GetVelocity();
+        public float GetHpPercent();
+        public int DealDamage(int damage);
+        public void ApplyForce(Vector2 forceDir);
+        public void SetTargetPosition(Vector2 position, float percentToPerform = 1);
 
-        public abstract void SetAsGrabbing(IInteractableBody grabbedBody);
-        public abstract void SetAsGrabbed(IInteractableBody grabberBody, Vector2 grabPosition);
-        public abstract bool IsGrabbing();
-        public abstract bool IsGrabbed();
-        public abstract IInteractableBody GetGrabbedTarget();
+        public void SetAsGrabbing(IInteractableBody grabbedBody);
+        public void SetAsGrabbed(IInteractableBody grabberBody, Vector2 grabPosition);
+        public bool IsGrabbing();
+        public bool IsGrabbed();
+        public IInteractableBody GetGrabbedTarget();
     }
 
 }
