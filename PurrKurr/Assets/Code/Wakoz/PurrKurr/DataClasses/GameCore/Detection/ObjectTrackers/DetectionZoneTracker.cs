036b3aa7fa1fa6f6a7bc21650ca8cb23d8d0fa69
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2025-01-26 02:48:05 +0200

    Tracker for nearby objects

---
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/DataClasses/GameCore/Detection/ObjectTrackers/DetectionZoneTracker.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Linq;
+using UnityEngine;
+
+namespace Code.Wakoz.PurrKurr.DataClasses.GameCore.Detection
+{
+    public class DetectionZoneTracker : MonoBehaviour
+    {
+        [SerializeField] private DetectionZone _zone;
+
+        [Tooltip("The transform to move within the radius")]
+        [SerializeField] protected Transform _trackerTransform;
+
+        [Tooltip("Time in seconds to refresh the closest target to focus on\ndefault 0 = constant refresh rate")]
+        [SerializeField] private float _refreshDurationInSeconds = 0.4f;
+
+        [Tooltip("Movement speed within the radius")]
+        [SerializeField][Min(0.1f)] private float _trackingSpeed = 2f;
+
+        [Tooltip("Max radius to move inside")]
+        [SerializeField][Min(0.1f)] protected float _maxRadius = 1f;
+
+        private Coroutine _trackingCoroutine;
+        private Collider2D _closestTarget;
+        private float _closestTargetDistance;
+
+        protected float GetClosestTargetDistance()
+            => _closestTargetDistance;
+
+        protected bool HasAnyTargetBelowDistance(float distance) 
+            => _closestTargetDistance < distance;
+
+        protected void Awake()
+        {
+            Bind();
+            StartTracking();
+        }
+
+        protected void OnDestroy()
+        {
+            Unbind();
+            StopTracking();
+        }
+
+        private void Bind()
+        {
+            if (_zone == null)
+                return;
+
+            _zone.OnColliderEntered += HandleColliderEntered;
+            _zone.OnColliderExited += HandleColliderExited;
+        }
+
+        private void Unbind()
+        {
+            if (_zone == null)
+                return;
+
+            _zone.OnColliderEntered -= HandleColliderEntered;
+            _zone.OnColliderExited -= HandleColliderExited;
+        }
+
+        protected virtual void HandleColliderEntered(Collider2D triggeredCollider)
+        {
+            if (_zone.GetColliders().Length == 1)
+            {
+                StartTracking();
+            }
+        }
+
+        protected virtual void HandleColliderExited(Collider2D triggeredCollider)
+        {
+            if (_zone.GetColliders().Length == 0)
+            {
+                StopTracking();
+            }
+
+        }
+        private void StartTracking()
+        {
+            if (_trackingCoroutine != null)
+            {
+                StopCoroutine(_trackingCoroutine);
+            }
+            _trackingCoroutine = StartCoroutine(TrackClosestCollider());
+        }
+
+        private void StopTracking()
+        {
+            if (_trackingCoroutine != null)
+            {
+                StopCoroutine(_trackingCoroutine);
+                _trackingCoroutine = null;
+            }
+        }
+
+        private IEnumerator TrackClosestCollider()
+        {
+            float elapsedTime = _refreshDurationInSeconds;
+            _closestTarget = null;
+
+            while (true)
+            {
+                elapsedTime += Time.deltaTime;
+
+                if (elapsedTime >= _refreshDurationInSeconds)
+                {
+                    elapsedTime = 0f;
+
+                    var targetsOrderedByClosestDistance = _zone.GetColliders().
+                    OrderBy(obj => (obj.transform.position - transform.position).sqrMagnitude).ToArray();
+
+                    _closestTarget = targetsOrderedByClosestDistance.FirstOrDefault();
+                }
+
+                TrackTarget();
+
+                yield return null;
+            }
+        }
+
+        protected virtual void TrackTarget()
+        {
+            var endPosition = _closestTarget != null ? _closestTarget.transform.position : transform.position;
+            UpdateClosestTargetDistance(endPosition);
+            
+            if (!HasAnyTargetBelowDistance(_maxRadius))
+            {
+                endPosition = transform.position + (endPosition - transform.position).normalized * _maxRadius;
+            }
+
+            LerpToTargetPosition(_trackerTransform, endPosition, _trackingSpeed);
+        }
+
+        private void UpdateClosestTargetDistance(Vector3 targetPos)
+        {
+            _closestTargetDistance = Vector2.Distance(transform.position, targetPos);
+            return;
+
+            // todo: check distance.sqrMagnitude as alternative for calculating radius
+            // var distance = endPosition - transform.position;
+            // Debug.Log($"squaredDistanceToTarget {_closestTargetDistance} <> {distance.sqrMagnitude} sqrMagnitude");
+        }
+
+        private void LerpToTargetPosition(Transform target, Vector3 endPosition, float speed)
+        {
+            target.position = Vector2.Lerp(target.position, endPosition, speed * Time.deltaTime);
+        }
+    }
+}
---
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/DataClasses/GameCore/Detection/ObjectTrackers/EyeballTracker.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+
+namespace Code.Wakoz.PurrKurr.DataClasses.GameCore.Detection
+{
+    public class EyeballTracker : DetectionZoneTracker
+    {
+        [SerializeField][Min(0.1f)] private float _eyeDiaationSpeed = 4f;
+        [SerializeField][Range(0, 1)] private float _diaalationMinScale = 0.5f;
+        [SerializeField][Range(0, 1)] private float _diaalationMaxScale = 0.9f;
+        [Tooltip("Additional Radius beyond maxRadius to start diaalite process")]
+        [SerializeField][Min(0f)] private float _diaalationRadius = .5f;
+
+        private float _maxDistance = 1f;
+
+        protected override void TrackTarget()
+        {
+            base.TrackTarget();
+
+            LerpToTargetScale(_trackerTransform, GetDiaalationScaleByDistanceToClosestTarget(), _eyeDiaationSpeed);
+        }
+
+        protected override void HandleColliderEntered(Collider2D triggeredCollider)
+        {
+            base.HandleColliderEntered(triggeredCollider);
+
+            UpdateMaxDistance(triggeredCollider.transform.position);
+        }
+
+        private void LerpToTargetScale(Transform target, Vector3 endScale, float speed)
+        {
+            target.localScale = Vector2.Lerp(target.localScale, endScale, speed * Time.deltaTime);
+        }
+
+        private Vector3 GetDiaalationScaleByDistanceToClosestTarget()
+        {
+            /*var newDiaalationScale =
+                (HasAnyObjectsBelowDistance(_diaalationRadius) ? _diaalationMaxScale : _diaalationMinScale)
+                * Vector3.one;*/
+            var newDiaalationScale = _diaalationMaxScale;
+
+            if (!HasAnyTargetBelowDistance(_diaalationRadius))
+            {
+                var closestTargetPos = GetClosestTargetDistance();
+
+                var percentage = 1 - ((closestTargetPos - _diaalationRadius) / (_maxDistance - _diaalationRadius));
+
+                newDiaalationScale = ((_diaalationMaxScale - _diaalationMinScale) * percentage + _diaalationMinScale);
+            } 
+
+            return newDiaalationScale * Vector3.one;
+        }
+
+        private void UpdateMaxDistance(Vector3 position)
+        {
+            var newDistance = (transform.position - position).magnitude;
+            if (newDistance > _maxDistance)
+            {
+                _maxDistance = newDistance;
+            }
+        }
+    }
+}
