9d5779ece3d4ba3b0e11f6734e52bf26f1a9eb93
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2025-01-26 02:53:43 +0200

    format

--- PurrKurr/Assets/Code/Wakoz/PurrKurr/DataClasses/GameCore/Detection/DetectionZoneTrigger.cs
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/DataClasses/GameCore/Detection/DetectionZoneTrigger.cs
@@ -3,54 +3,58 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Code.Wakoz.PurrKurr.DataClasses.GameCore.Detection
 {
 
     [DefaultExecutionOrder(15)]
-    public class DetectionZoneTrigger : Controller {
-
+    public class DetectionZoneTrigger : Controller
+    {
         public event Action<Collider2D> OnColliderEntered;
         public event Action<Collider2D> OnColliderExited;
 
-        [SerializeField] private List<OverlayWindowData> overlayWindowData;
+        [FormerlySerializedAs("overlayWindowData")]
+        [SerializeField] private List<OverlayWindowData> _overlayWindowData;
 
-        [SerializeField] private DetectionZone zone;
+        [FormerlySerializedAs("zone")]
+        [SerializeField] private DetectionZone _zone;
 
-        public List<OverlayWindowData> GetWindowData() => overlayWindowData;
+        public List<OverlayWindowData> GetWindowData() => _overlayWindowData;
 
-        public void UpdateWindowData(List<OverlayWindowData> data) => overlayWindowData = data;
+        public void UpdateWindowData(List<OverlayWindowData> data) => _overlayWindowData = data;
 
-        protected override void Clean() {
-
-            if (zone == null) {
+        protected override void Clean()
+        {
+            if (_zone == null)
+            {
                 return;
             }
-            
-            zone.OnColliderEntered -= handleColliderEntered;
-            zone.OnColliderExited -= handleColliderExited;
+
+            _zone.OnColliderEntered -= handleColliderEntered;
+            _zone.OnColliderExited -= handleColliderExited;
         }
 
-        protected override Task Initialize() {
-
+        protected override Task Initialize()
+        {
             Init();
 
             return Task.CompletedTask;
         }
 
-        private void Init() {
-
-            if (zone == null) {
+        private void Init()
+        {
+            if (_zone == null)
+            {
                 return;
             }
 
-            zone.OnColliderEntered += handleColliderEntered;
-            zone.OnColliderExited += handleColliderExited;
+            _zone.OnColliderEntered += handleColliderEntered;
+            _zone.OnColliderExited += handleColliderExited;
         }
-
-        private void handleColliderExited(Collider2D triggeredCollider) => OnColliderExited?.Invoke(triggeredCollider);
 
         private void handleColliderEntered(Collider2D triggeredCollider) => OnColliderEntered?.Invoke(triggeredCollider);
 
+        private void handleColliderExited(Collider2D triggeredCollider) => OnColliderExited?.Invoke(triggeredCollider);
     }
 }
