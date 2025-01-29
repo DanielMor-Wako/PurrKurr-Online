7edae0b6e929ec1c09449803e07c40d66b6615bc
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2025-01-29 11:18:27 +0200

    Camera focuses on a spot within a dedicated zone

---
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/DataClasses/GameCore/Detection/CameraTrigger/CameraZoneNotifier.cs
@@ -0,0 +1,26 @@
+﻿using Code.Wakoz.PurrKurr.Screens.CameraSystem;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Code.Wakoz.PurrKurr.DataClasses.GameCore.Detection
+{
+    public class CameraZoneNotifier : MonoBehaviour, ICameraZoneNotifier
+    {
+        [Header("Zone - Camera Data")]
+
+        [Tooltip("Lifecycle when zone is active")]
+        [SerializeField] private LifeCycleData _lifeCycle;
+
+        [Tooltip("Transition settings while moving towards a new position")]
+        [SerializeField] private CameraTransitionsData _transition;
+
+        [Tooltip("Offset settings towards the gameobject transform position")]
+        [SerializeField] private CameraOffsetData _offset;
+
+        public CameraData GetCameraData()
+        {
+            return new CameraData(new List<Transform>() { transform },
+                        _lifeCycle, _transition, _offset);
+        }
+    }
+}
---
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/DataClasses/GameCore/ICameraZoneNotifier.cs
@@ -0,0 +1,9 @@
+﻿using Code.Wakoz.PurrKurr.Screens.CameraSystem;
+
+namespace Code.Wakoz.PurrKurr.DataClasses.GameCore
+{
+    public interface ICameraZoneNotifier
+    {
+        CameraData GetCameraData();
+    }
+}
