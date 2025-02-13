20febc1a2bb48a20029ff2ec8bbb5a81f078331e
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2025-02-13 03:19:37 +0200

    Countdown based on game timescale

--- PurrKurr/Assets/Code/Wakoz/PurrKurr/Views/OnStartEvent.cs
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/Views/OnStartEvent.cs
@@ -1,23 +1,72 @@
-using System.Threading.Tasks;
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
 namespace Code.Wakoz.PurrKurr.Views {
+        
     public class OnStartEvent : View {
         
         public UnityEvent OnStart;
-        
+
+        [Tooltip("Delay before action, in Milliseconds")]
         [SerializeField][Range(0, 10000)]
-        private int _preActionDelay;
+        private float _preActionDelay;
+
+        private Coroutine _countDownCoroutine;
 
         protected override void ModelChanged() {}
-        
-        private async void Awake()
+
+        private void Awake()
         {
-            await Task.Delay(_preActionDelay);
-            OnStart?.Invoke();
+            if (OnStart == null || OnStart.GetPersistentEventCount() == 0)
+            {
+                Debug.LogWarning($"OnStartEvent component exist but has not events {gameObject.name}");
+                return;
+            }
+
+            if (_preActionDelay == 0) 
+            {
+                OnStart?.Invoke();
+                return;
+            }
+
+            StartCountdown();
         }
 
+        private void StartCountdown()
+        {
+            if (_countDownCoroutine != null)
+            {
+                StopCoroutine(_countDownCoroutine);
+            }
+            _countDownCoroutine = StartCoroutine(InvokeOnStartWithDelay());
+        }
+
+        private void StopCountdown()
+        {
+            if (_countDownCoroutine != null)
+            {
+                StopCoroutine(_countDownCoroutine);
+                _countDownCoroutine = null;
+            }
+        }
+
+        private IEnumerator InvokeOnStartWithDelay()
+        {
+            var elapsedTime = 0f;
+            var seconds = _preActionDelay / 1000;
+
+            while (true && elapsedTime <= seconds)
+            {
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            OnStart?.Invoke();
+
+            StopCountdown();
+        }
     }
 
 }
