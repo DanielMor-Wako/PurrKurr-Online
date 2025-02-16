cb0b624dbeed60db8c015f02ffc33df4ab66f85c
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2025-02-16 10:56:59 +0200

    LevelController as a holder for objectives

--- PurrKurr/Assets/Code/Wakoz/PurrKurr/Screens/Levels/LevelController.cs
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/Screens/Levels/LevelController.cs
@@ -1,6 +1,4 @@
-﻿using Code.Wakoz.PurrKurr.DataClasses.Objectives;
-using System.Collections.Generic;
-using System;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Code.Wakoz.PurrKurr.DataClasses.ScriptableObjectData;
 using UnityEngine;
@@ -11,102 +9,16 @@ namespace Code.Wakoz.PurrKurr.Screens.Levels {
 
         [SerializeField] private List<ObjectiveDataSO> _objectivesData;
 
-        private ObjectivesManager _objectivesManager;
+        public List<ObjectiveDataSO> GetObjectivesData() 
+            => _objectivesData;
 
+        protected override void Clean() { }
 
-        protected override void Clean() {
-
-        }
-
-        protected override Task Initialize() {
-
+        protected override Task Initialize()
+        {
             return Task.CompletedTask;
         }
 
-        public void CompleteObjectiveOfType(Type type) {
-
-            if (_objectivesManager == null || _objectivesData.Count == 0) {
-                return;
-            }
-
-            _objectivesManager.CompleteObjectiveOfType(type);
-        }
-
-        public void UpdateObjectiveOfCollectableType(string collectableType, int amountToAdd) {
-
-            if (_objectivesManager == null || _objectivesData.Count == 0) {
-                return;
-            }
-
-            _objectivesManager.UpdateObjectiveOfCollectableType(collectableType, amountToAdd);
-        }
-
-        public void InitObjectivesManager(ref List<string> completedObjectivesId) {
-
-            var objectivesList = new List<IObjective>();
-            foreach (var data in _objectivesData) {
-                // todo: use objective id for marking something as done?
-                if (completedObjectivesId.Contains(data.uniqueId)) {
-                    Debug.Log($"objective {data.uniqueId} marked as completed");
-                    continue;
-                }
-                IObjective objective = CreateObjective(data);
-                if (objective != null) {
-                    objective.Initialize(data);
-                    objectivesList.Add(objective);
-                }
-            }
-
-            _objectivesManager ??= new ObjectivesManager();
-            _objectivesManager.Initialize(objectivesList, ref completedObjectivesId);
-        }
-
-        private IObjective CreateObjective(ObjectiveDataSO data) {
-
-            Type objectiveType = GetObjectiveType(data);
-            if (objectiveType != null) {
-                if (typeof(IObjective).IsAssignableFrom(objectiveType)) {
-                    IObjective objective = (IObjective)Activator.CreateInstance(objectiveType);
-                    return objective;
-                } else {
-                    Debug.LogError("Invalid objective type: " + objectiveType.Name);
-                }
-            } else {
-                Debug.LogError("Failed to determine objective type for: " + data.GetType().Name);
-            }
-            return null;
-        }
-
-        private Type GetObjectiveType(ObjectiveDataSO data) {
-
-            Type objectiveDataType = data.GetType();
-            var attribute = (TypeMarkerMultiClassAttribute)Attribute.GetCustomAttribute(objectiveDataType, typeof(TypeMarkerMultiClassAttribute));
-
-            if (attribute != null) {
-                return attribute.type;
-            } else {
-                Debug.LogError("No ObjectiveTypeAttribute found for ObjectiveDataSO: " + objectiveDataType.Name);
-                return null;
-            }
-        }
-
-        public void UpdateObjectives() {
-
-            if (_objectivesManager == null) {
-                return;
-            }
-
-            _objectivesManager.UpdateObjectives();
-        }
-
-        public List<string> GetCompletedObjectivesUniqueId() {
-
-            if (_objectivesManager == null) {
-                return null;
-            }
-
-            return _objectivesManager.GetObjectives();
-        }
     }
 
 }
--- PurrKurr/Assets/Code/Wakoz/PurrKurr/Screens/Levels/LevelsController.cs
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/Screens/Levels/LevelsController.cs
@@ -16,20 +16,18 @@ using System.Threading.Tasks;
 using UnityEngine;
 
 namespace Code.Wakoz.PurrKurr.Screens.Levels {
-    [DefaultExecutionOrder(12)]
 
+    [DefaultExecutionOrder(12)]
     public class LevelsController : SingleController {
 
         [SerializeField] private List<LevelController> _levelControllers;
         [SerializeField] private MultiStateView _levels;
 
-        List<string> _completeObjectives = new();
-
         private InteractablesController _interactablesPool;
-        public int _currentLevelIndex = -1;
+        private int _currentLevelIndex = -1;
         private PersistentGameObjectsManager _persistentGameObjectsManager;
 
-        private GameplayController _gameplayController;
+        public int CurrentLevelIndex => _currentLevelIndex;
 
         public void InitInteractablePools(InteractablesController interactablesPool) {
 
@@ -39,82 +37,28 @@ namespace Code.Wakoz.PurrKurr.Screens.Levels {
             _interactablesPool.CreateObjectPool(_interactablesPool._ropes.FirstOrDefault(), 0, 1, "Ropes");
         }
 
-        public void BindEvents(GameplayController gameplayController)
-        {
-            UnBindEvents();
-            _gameplayController = gameplayController;
-            _gameplayController.OnHeroEnterDetectionZone += HandleHeroEnterDetectionZone;
-        }
-
-        private void UnBindEvents()
-        {
-            if (_gameplayController == null) {
-                return;
-            }
-            _gameplayController.OnHeroEnterDetectionZone -= HandleHeroEnterDetectionZone;
-            _gameplayController = null;
-        }
-
-        private void HandleHeroEnterDetectionZone(DetectionZoneTrigger zone) {
-
-            switch (zone) {
-                case DoorController: {
-
-                        var door = zone as DoorController;
-
-                        if (!door.IsDoorEntrance()) {
-                            // _currentLevelIndex ??= door.GetNextDoor().GetRoomIndex()
-                            CompleteObjectiveOfType(_currentLevelIndex, typeof(ReachTargetZoneObjective));
-                        }
-
-                        break;
-                    }
-
-                case CollectableItemController: {
-
-                        var collectableItem = zone as CollectableItemController;
-                        
-                        UpdateObjectiveOfCollectableType(_currentLevelIndex, collectableItem.GetItemId(), collectableItem.GetItemQuantity());
-                        
-                        break;
-                    }
-
-                case not OverlayWindowTrigger:
-                    Debug.Log("No explicit interpreter for " + zone.GetType());
-                    break;
-            }
-        }
-        // todo: make the classes that derive from DetectionZoneTrigger, use the TypeMarkerMultiClassAttribute to mark the class,
-        // like in CollectableItemController or DoorController
-        /*private void HandleHeroEnterDetectionZone(DetectionZoneTrigger zone) {
-            Type zoneType = zone.GetType();
-            Type objectiveType = GetObjectiveTypeForZone(zoneType);
-
-            if (objectiveType != null) {
-                CompleteObjective(_currentLevelIndex, objectiveType);
-            } else if (zone is not OverlayWindowTrigger) {
-                Debug.Log("No explicit interpreter for " + zoneType);
-            }
-        }
-
-        private Type GetObjectiveTypeForZone(Type zoneType) {
-            var attributes = zoneType.GetCustomAttributes(typeof(TypeMarkerMultiClassAttribute), true);
-            if (attributes.Length > 0) {
-                var attribute = (TypeMarkerMultiClassAttribute)attributes[0];
-                return attribute.type;
-            }
-            return null;
-        }*/
-
         public void LoadLevel(int levelToLoad) {
-
-            //var levelData = GetLevelData(levelToLoad);
 
             _currentLevelIndex = levelToLoad;
 
             SetSingleLevel(levelToLoad);
+        }
 
-            InitLevel(levelToLoad);
+        public LevelController GetLevel(int index)
+        {
+            if (_levelControllers == null)
+            {
+                Debug.LogError("Level Controllers has no ref");
+                return null;
+            }
+
+            if (index < 0 || index >= _levelControllers.Count)
+            {
+                Debug.LogError($"Error no level with Index {index}");
+                return null;
+            }
+
+            return _levelControllers[index];
         }
 
         public void RefreshSpritesOrder(Character2DController mainHero) {
@@ -141,18 +85,11 @@ namespace Code.Wakoz.PurrKurr.Screens.Levels {
             character.SetSpriteOrder(isMainHero ? 2 : isAlive ? 1 : 0);
         }
 
-
         protected override void Clean() {
 
             CleanPersistentGameObjectsManager();
 
-            UnBindEvents();
-
-            if (_interactablesPool == null) {
-                return;
-            }
-
-            // todo? remove created pools and their objects?
+            CleanInteractablesPool();
         }
 
         protected override Task Initialize() {
@@ -162,11 +99,24 @@ namespace Code.Wakoz.PurrKurr.Screens.Levels {
             return Task.CompletedTask;
         }
 
-        private async Task GetLevelData(int levelToLoad) {
-            
-            if (_currentLevelIndex > -1) {
-                // Unload previous level
+        private void InitPersistentGameObjectsManager()
+        {
+            _persistentGameObjectsManager ??= new PersistentGameObjectsManager();
+        }
+
+        private void CleanPersistentGameObjectsManager()
+        {
+            _persistentGameObjectsManager.Dispose();
+        }
+
+        private void CleanInteractablesPool()
+        {
+            if (_interactablesPool == null)
+            {
+                return;
             }
+
+            // todo? remove created pools and their objects
         }
 
         private void SetSingleLevel(int levelToLoad) {
@@ -178,88 +128,5 @@ namespace Code.Wakoz.PurrKurr.Screens.Levels {
             _levels.ChangeState(levelToLoad);
         }
 
-        private void InitLevel(int levelToLoad) {
-
-            if (_levelControllers == null) {
-                return;
-            }
-
-            if (levelToLoad >= 0 && levelToLoad < _levelControllers.Count) {
-                var levelController = _levelControllers[levelToLoad];
-                if (levelController != null)
-                {
-                    Debug.Log("Initializing level " + levelToLoad);
-
-                    //var completedObjectives = _completeObjectives[levelToLoad];
-                    levelController.InitObjectivesManager(ref _completeObjectives);
-                } else {
-                    Debug.LogError("ObjectiveManagerScript for level " + levelToLoad + " is missing or null.");
-                }
-            }
-            else
-            {
-                Debug.LogError("Invalid level index: " + levelToLoad);
-            }
-        }
-        
-        public List<string> GetCompletedObjectivesUniqueId(int levelIndex) {
-            
-            return _levelControllers[levelIndex].GetCompletedObjectivesUniqueId();
-        }
-
-        /*private void FixedUpdate() {
-
-            _levelControllers[_currentLevelIndex].UpdateObjectives();
-        }*/
-
-        private void InitPersistentGameObjectsManager() {
-
-            _persistentGameObjectsManager ??= new PersistentGameObjectsManager();
-        }
-
-        private void CleanPersistentGameObjectsManager() {
-
-            _persistentGameObjectsManager.Dispose();
-        }
-
-        public void UpdateObjectiveOfCollectableType(int levelIndex, string collectableType, int amountToAdd) {
-
-            var levelController = GetLevelController(levelIndex);
-
-            if (levelController == null) {
-                return;
-            }
-
-            levelController.UpdateObjectiveOfCollectableType(collectableType, amountToAdd);
-
-            levelController.UpdateObjectives();
-        }
-
-        public void CompleteObjectiveOfType(int levelIndex, Type type) {
-
-            var levelController = GetLevelController(levelIndex);
-
-            if (levelController == null) {
-                return;
-            }
-            
-            levelController.CompleteObjectiveOfType(type);
-
-            levelController.UpdateObjectives();
-        }
-
-        private LevelController GetLevelController(int levelIndex) {
-
-            LevelController levelController = null;
-            if (levelIndex >= 0 && levelIndex < _levelControllers.Count) {
-                levelController = _levelControllers[levelIndex];
-            }
-
-            if (levelController == null) {
-                Debug.LogError($"no level controller with Index {levelIndex}");
-            }
-
-            return levelController;
-        }
     }
 }
