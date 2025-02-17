1b7dc3af6934c8f6f89e29cbeb629c97eec43d94
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2025-02-17 17:27:27 +0200

    Absract Factory pattern

---
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/StrategyPatterns/FactoryPattern.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+
+namespace Code.Wakoz.PurrKurr.StrategyPatterns
+{
+
+    public abstract class FactoryPattern { }
+
+    /// <summary>
+    /// Generic interface for all factories.
+    /// </summary>
+    public interface IFactory<T> where T : class
+    {
+        /// <summary>
+        /// Creates an instance of the specified type.
+        /// </summary>
+        T Create(Type type);
+    }
+
+    /// <summary>
+    /// Abstract base class for factories, providing a default implementation for object creation.
+    /// </summary>
+    public abstract class FactoryPattern<T> : IFactory<T> where T : class
+    {
+        /// <summary>
+        /// Creates and returns an instance of the specified type
+        /// </summary>
+        public virtual T Create(Type type)
+        {
+            if (type == null)
+            {
+                Debug.LogError("Type cannot be null.");
+                return default;
+            }
+
+            if (!typeof(T).IsAssignableFrom(type))
+            {
+                Debug.LogError($"Type {type.Name} does not implement {typeof(T).Name}.");
+                return default;
+            }
+
+            try
+            {
+                return (T)Activator.CreateInstance(type);
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError($"Failed to create instance of type {type.Name}: {ex.Message}");
+                return default;
+            }
+        }
+    }
+}
