9fc8895f453df5c4c6f2e847df1787adc6a0c11f
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2025-02-19 16:38:14 +0200

    moved sort logic to controller

--- PurrKurr/Assets/Code/Wakoz/PurrKurr/Screens/Objectives/ObjectivesModel.cs
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/Screens/Objectives/ObjectivesModel.cs
@@ -42,7 +42,7 @@ namespace Code.Wakoz.PurrKurr.Screens.Objectives
         /// <summary>
         /// Executes the sorting logic if a SortAction is provided.
         /// </summary>
-        public void ReorderByUniqueIdsInternal(List<string> uniqueIds)
+        public void ReorderItemsInternal(List<string> uniqueIds)
         {
             if (SortAction == null)
             {
@@ -50,35 +50,6 @@ namespace Code.Wakoz.PurrKurr.Screens.Objectives
             }
 
             SortAction.Invoke(uniqueIds);
-        }
-
-        public void ReorderItemsInternal(List<string> uniqueIds)
-        {
-            if (uniqueIds == null)
-                throw new ArgumentNullException(nameof(uniqueIds));
-
-            if (Objectives == null || Objectives.Count == 0)
-                return;
-
-            // Create a dictionary to map unique IDs to their order
-            var idOrder = uniqueIds
-                .Select((id, index) => new { id, index })
-                .ToDictionary(x => x.id, x => x.index);
-
-            // Sort the Objectives list in place based on the order in uniqueIds
-            Objectives.Sort((o1, o2) =>
-            {
-                if (o1 == null || o2 == null)
-                    return 0;
-
-                var id1 = o1.InterfaceData.GetUniqueId();
-                var id2 = o2.InterfaceData.GetUniqueId();
-
-                var index1 = idOrder.ContainsKey(id1) ? idOrder[id1] : int.MaxValue;
-                var index2 = idOrder.ContainsKey(id2) ? idOrder[id2] : int.MaxValue;
-
-                return index1.CompareTo(index2);
-            });
         }
 
     }
