2b6b3ef63a3f1b60cd885c14fbbce39620ca7993
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2025-02-23 05:24:02 +0200

    Data holder for collectable item

---
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/DataClasses/GameCore/CollectableItems/CollectableItemData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Code.Wakoz.PurrKurr.DataClasses.GameCore.CollectableItems
+{
+
+    [Serializable]
+    public class CollectableItemData
+    {
+        [SerializeField] private string _itemId = "";
+
+        [SerializeField][Min(1)] private int _quantity = 1;
+
+        public string ItemId => _itemId;
+        public int Quantity => _quantity;
+    }
+}
