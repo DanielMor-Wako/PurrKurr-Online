7833732e5a708016230015699cedbaebfc398c45
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2025-03-12 03:01:33 +0200

    agent goal data

---
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/Agents/GoalData.cs
@@ -0,0 +1,21 @@
+﻿using Code.Wakoz.PurrKurr.DataClasses.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace Code.Wakoz.PurrKurr.Agents
+{
+    [System.Serializable]
+    public class GoalData
+    {
+        public Definitions.AgentGoal Goal;
+        public List<string> TargetStrings; // List of target strings for the goal
+        public List<GoalConditionData> Conditions;
+    }
+
+    [System.Serializable]
+    public class GoalConditionData
+    {
+        public Definitions.GoalCondition Condition;
+        public float targetValue;
+    }
+}
