f3a0c9e6c7d3e93795e048dbfcfb992415bb8ecf
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2025-03-14 20:03:41 +0200

    agents attacking

--- PurrKurr/Assets/Code/Wakoz/PurrKurr/Agents/AgentsHandler.cs
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/Agents/AgentsHandler.cs
@@ -2,6 +2,7 @@
 using Code.Wakoz.PurrKurr.DataClasses.Enums;
 using Code.Wakoz.PurrKurr.DataClasses.GameCore;
 using Code.Wakoz.PurrKurr.DataClasses.GameCore.ConsumeableItems;
+using Code.Wakoz.PurrKurr.Screens.Gameplay_Controller;
 using Code.Wakoz.PurrKurr.Screens.Gameplay_Controller.Handlers;
 using Code.Wakoz.PurrKurr.Screens.Levels;
 using System.Collections.Generic;
@@ -65,9 +66,12 @@ namespace Code.Wakoz.PurrKurr.Agents
                     foreach (var agent in _agentsPool)
                     {
                         if (agent == null)
-                        {
                             continue;
-                        }
+
+                        var characterAgent = agent.CharacterController;
+                        if (characterAgent != null && characterAgent.GetHpPercent() == 0 && characterAgent.GetVelocity().magnitude < 0.01f)
+                            continue;
+
                         _agentQueue.Enqueue(agent);
                     }
                 }
@@ -97,36 +101,161 @@ namespace Code.Wakoz.PurrKurr.Agents
             _currentAgent = null;
         }
 
+        private void GenerateAndExecutePlan(AgentController agent, GoalData goalData)
+        {
+            var plan = CreatePlan(agent, goalData);
+
+            ExecutePlan(agent, plan, goalData.Goal);
+        }
+
+        private (float xMove, float yForce) CreatePlan(AgentController agent, GoalData goal)
+        {
+            var character = agent.CharacterController;
+
+            if (character == null || character.GetHpPercent() == 0)
+                return (0, 0);
+
+            var characterPosition = character.GetCenterPosition();
+
+            switch (goal.Goal)
+            {
+                case Definitions.AgentGoal.Explore:
+
+                var consumables = GetClosestTargets(goal.TargetStrings, characterPosition);
+                if (consumables.Count == 0)
+                {
+                    return (character.Stats.WalkSpeed * character.State.GetFacingRightAsInt(), 0);
+                }
+                var exploreSpeed = character.Stats.WalkSpeed;
+                var closestConsumable = consumables.FirstOrDefault();
+                var dirToNearbyObject = (closestConsumable.position - characterPosition);
+                var distanceToConsumbeable = Vector2.Distance(characterPosition, closestConsumable.position);
+                var senses = character.Senses?.GetComponent<CircleCollider2D>();
+                if (senses != null && distanceToConsumbeable < senses.radius)
+                {
+                    exploreSpeed = character.Stats.RunSpeed;
+                    if (dirToNearbyObject.y > 0 && character.State.IsTouchingAnySurface())
+                    {
+                        return (character.Stats.JumpForce*.25f * dirToNearbyObject.normalized.x, character.Stats.JumpForce*.75f);
+                    }
+                }
+                if (character.State.IsFrontWall() && character.GetVelocity().magnitude < 3)
+                {
+                    exploreSpeed *= -1;
+                    character.FlipCharacterTowardsPoint(-exploreSpeed > 0);
+                }
+                var hasReachedZone = dirToNearbyObject.magnitude < 2; // 2 is the minimum distance to the consumeable
+                if (hasReachedZone)
+                {
+                    Debug.Log($"{agent.name} consuming {closestConsumable.name}");
+                    var consumeable = closestConsumable.GetComponent<ConsumeableItemController>();
+                    if (consumeable != null)
+                    {
+                        consumeable.Consume(1);
+                    }
+                }
+                return (-exploreSpeed * dirToNearbyObject.normalized.x, 0);
+
+                case Definitions.AgentGoal.Protect:
+                var protectee = GetClosestTargets(goal.TargetStrings, characterPosition);
+                if (protectee.Count == 0)
+                {
+                    return (0, 0);
+                }
+                var closestProtectee = protectee.FirstOrDefault();
+                var dirToClosestProtecee = (closestProtectee.position - characterPosition);
+                return (-character.Stats.RunSpeed * dirToClosestProtecee.normalized.x, 0);
+
+                case Definitions.AgentGoal.Fight:
+
+                var nearbyCharacters = GetNearbyAttackers(agent);
+                var foe = nearbyCharacters?.FirstOrDefault();
+                if (foe == null)
+                {
+                    return (0, 0);
+                }
+                var dirToFoe = character.GetCenterPosition() - foe.GetCenterPosition();
+                var hasReachedFoe = dirToFoe.magnitude < 5; // 2 is the minimum distance to attack
+                if (hasReachedFoe && character.State.CanPerformAction() && Random.Range(0, 100) < 4)
+                {
+                    Debug.Log($"{agent.name} attacking {foe.name}");
+                    SingleController.GetController<GameplayController>().CombatLogic(character, Definitions.ActionType.Attack, foe.GetCenterPosition(), character.NearbyInteractables());
+                }
+                return (dirToFoe.normalized.x * character.Stats.RunSpeed, 0);
+
+                case Definitions.AgentGoal.Run:
+
+                var nearbyInteractables = GetNearbyAttackers(agent);
+                var attacker = nearbyInteractables?.FirstOrDefault();
+                if (attacker == null)
+                {
+                    return (character.State.GetFacingRightAsInt() * character.Stats.RunSpeed, 0);
+                }
+                return ((attacker.GetCenterPosition() - character.GetCenterPosition()).normalized.x * character.Stats.SprintSpeed, 0);
+            }
+
+            return (0, 0);
+        }
+
+        private void ExecutePlan(AgentController agent, (float xMove, float yForce) plan, Definitions.AgentGoal goal)
+        {
+            // Set goal for display in the editor and inspector
+            agent.CurrentGoal(goal);
+
+            // Execute the plan on the agent's controller
+            agent.CharacterController.DoMove(plan.xMove);
+            if (plan.yForce > 0)
+            {
+                agent.CharacterController.SetForceDir(new Vector2(plan.xMove, plan.yForce));
+            }
+        }
+
+        private List<Transform> GetClosestTargets(List<string> targets, Vector3 point)
+        {
+            var res = new List<Transform>();
+
+            res.AddRange(_levelsController.GetTaggedObject(targets));
+
+            return res.OrderBy(obj => Vector2.Distance(obj.transform.position, point)).ToList();
+        }
+
         private GoalData GetHighestPriorityGoal(AgentController agent)
         {
             var goals = agent.Goals;
 
             foreach (var goal in goals)
-            {
-                if (IsValidGoal(goal.Conditions, agent))
-                {
+                if (IsValidGoal(goal, agent))
                     return goal;
-                }
-            }
 
             return goals.FirstOrDefault();
         }
 
-        private bool IsValidGoal(List<GoalConditionData> conditions, AgentController agent)
+        private bool IsValidGoal(GoalData goal, AgentController agent)
         {
+            var conditions = goal.Conditions;
             foreach (var condition in conditions)
             {
                 switch (condition.Condition)
                 {
                     case Definitions.GoalCondition.HpIsAboveRange01:
-                        if (agent.CharacterController.GetHpPercent() < condition.targetValue)
-                            return false;
+                    if (agent.CharacterController.GetHpPercent() < condition.targetValue)
+                        return false;
                     break;
 
-                    case Definitions.GoalCondition.HasNearby:
-                        var nearbyInteractables = GetNearbyAttackers(agent);
-                        if (nearbyInteractables == null || nearbyInteractables.Count > condition.targetValue)
-                            return false;
+                    case Definitions.GoalCondition.HasNearbyAttackers:
+                    var nearbyInteractables = GetNearbyAttackers(agent);
+                    if (condition.targetValue == 0 && nearbyInteractables.Count > 0)
+                        return false;
+
+                    var hasMinimumAttackers = nearbyInteractables.Count >= condition.targetValue;
+                    if (!hasMinimumAttackers)
+                        return false;
+                    break;
+
+                    case Definitions.GoalCondition.HasNearbyConsumeables:
+                    var nearbyConsumeables = GetNearbyConsumeables(agent, goal);
+                    if (nearbyConsumeables.Count < condition.targetValue)
+                        return false;
                     break;
                 }
             }
@@ -172,109 +301,23 @@ namespace Code.Wakoz.PurrKurr.Agents
             return res;
         }
 
-        private void GenerateAndExecutePlan(AgentController agent, GoalData goalData)
+        private List<Transform> GetNearbyConsumeables(AgentController agent, GoalData goal)
         {
-            var plan = CreatePlan(agent, goalData);
-
-            ExecutePlan(agent, plan, goalData.Goal);
-        }
-
-        private (float xMove, float yForce) CreatePlan(AgentController agent, GoalData goal)
-        {
-            var character = agent.CharacterController;
-
-            if (character == null || character.GetHpPercent() == 0)
-                return (0, 0);
-
-            var characterPosition = character.GetCenterPosition();
-
-            switch (goal.Goal)
+            List<Transform> res = new();
+            var consumables = GetClosestTargets(goal.TargetStrings, agent.CharacterController.GetCenterPosition());
+            if (consumables.Count > 0)
             {
-                case Definitions.AgentGoal.Explore:
-
-                var consumables = GetClosestTargets(goal.TargetStrings, characterPosition);
-                if (consumables.Count == 0) {
-                    return (character.Stats.WalkSpeed * character.State.GetFacingRightAsInt(), 0);
-                }
-                var exploreSpeed = character.Stats.WalkSpeed;
-                var closestConsumable = consumables.FirstOrDefault();
-                var dirToNearbyObject = (closestConsumable.position - characterPosition);
-                var distanceToConsumbeable = Vector2.Distance(characterPosition, closestConsumable.position);
-                var senses = character.Senses?.GetComponent<CircleCollider2D>();
-                if (senses != null && distanceToConsumbeable < senses.radius)
+                foreach (var item in consumables)
                 {
-                    exploreSpeed = character.Stats.RunSpeed;
-                    if (dirToNearbyObject.y > 0 && character.State.IsTouchingAnySurface())
+                    var consumeableItem = item.GetComponent<ConsumeableItemController>();
+                    if (consumeableItem == null || consumeableItem.GetData().quantity == 0)
                     {
-                        return (character.Stats.JumpForce*.25f * dirToNearbyObject.normalized.x, character.Stats.JumpForce*.75f);
+                        continue;
                     }
+                    res.Add(item);
                 }
-                if (character.State.IsFrontWall() && character.GetVelocity().magnitude < 3)
-                {
-                    exploreSpeed *= -1;
-                    character.FlipCharacterTowardsPoint(-exploreSpeed > 0);
-                }
-                var canConsumeItem = dirToNearbyObject.magnitude < 2; // 2 is the minimum distance to the consumeable
-                if (canConsumeItem)
-                {
-                    Debug.Log($"{agent.name} consuming {closestConsumable.name}");
-                    var consumeable = closestConsumable.GetComponent<ConsumeableItemController>();
-                    if (consumeable != null)
-                    {
-                        consumeable.Consume(1);
-                    }
-                }
-                return (-exploreSpeed * dirToNearbyObject.normalized.x, 0);
-
-                case Definitions.AgentGoal.Protect:
-
-                return (0, 0);
-
-                case Definitions.AgentGoal.Fight:
-
-                var nearbyCharacters = GetNearbyAttackers(agent);
-                var foe = nearbyCharacters?.FirstOrDefault();
-                if (foe == null)
-                {
-                    return (0, 0);
-                }
-                var dirToFoe = character.GetCenterPosition() - foe.GetCenterPosition();
-                return (dirToFoe.normalized.x * character.Stats.RunSpeed, 0);
-
-                case Definitions.AgentGoal.Run:
-
-                var nearbyInteractables = GetNearbyAttackers(agent);
-                var attacker = nearbyInteractables?.FirstOrDefault();
-                if (attacker == null)
-                {
-                    return (character.State.GetFacingRightAsInt() * character.Stats.RunSpeed, 0);
-                }
-                return ((attacker.GetCenterPosition() - character.GetCenterPosition()).normalized.x * character.Stats.SprintSpeed, 0);
             }
-
-            return (0, 0);
-        }
-
-        private List<Transform> GetClosestTargets(List<string> targets, Vector3 point)
-        {
-            var res = new List<Transform>();
-
-            res.AddRange(_levelsController.GetTaggedObject(targets));
-
-            return res.OrderBy(obj => Vector2.Distance(obj.transform.position, point)).ToList();
-        }
-
-        private void ExecutePlan(AgentController agent, (float xMove, float yForce) plan, Definitions.AgentGoal goal)
-        {
-            // Set goal for the editor and inspector
-            agent.CurrentGoal(goal);
-
-            // Execute the plan on the agent's controller
-            agent.CharacterController.DoMove(plan.xMove);
-            if (plan.yForce > 0)
-            {
-                agent.CharacterController.SetForceDir(new Vector2(plan.xMove, plan.yForce));
-            }
+            return res;
         }
 
     }
