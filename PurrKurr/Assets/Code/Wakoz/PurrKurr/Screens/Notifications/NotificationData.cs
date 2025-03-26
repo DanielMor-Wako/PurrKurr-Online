1d7b5c49214303e5aa5a4d8248e3f9afa2414de4
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2025-03-26 22:24:48 +0200

    Notification Data - missing sprite as Icon

---
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/Screens/Notifications/NotificationData.cs
@@ -0,0 +1,14 @@
+﻿namespace Code.Wakoz.PurrKurr.Screens.Notifications
+{
+    public class NotificationData
+    {
+        public string Message { get; private set; }
+        public float Duration { get; private set; }
+
+        public NotificationData(string message, float duration) 
+        {
+            Message = message;
+            Duration = duration;
+        }
+    }
+}
