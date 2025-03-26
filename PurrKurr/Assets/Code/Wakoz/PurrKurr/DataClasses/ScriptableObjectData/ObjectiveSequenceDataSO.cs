06a901cbd77e6821a7bfc14ea1e12988cf53603d
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2025-03-26 22:14:54 +0200

    Update SO for mission sequence data

--- PurrKurr/Assets/Code/Wakoz/PurrKurr/DataClasses/ScriptableObjectData/ObjectiveSequenceDataSO.cs
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/DataClasses/ScriptableObjectData/ObjectiveSequenceDataSO.cs
@@ -9,13 +9,20 @@ namespace Code.Wakoz.PurrKurr.DataClasses.ScriptableObjectData
     {
         [SerializeField] private string _uniqueId;
 
+        [Tooltip("Finish conditions as Win or Lose during active mission")]
         [SerializeField] private ObjectiveFinishConditionsData _finishConditions;
 
+        [Tooltip("Instructions steps before mission starts the sequence data")]
+        [SerializeField] private List<ObjectiveSequenceData> _initializeData;
+
+        [Tooltip("Instructions steps for the mission as sequence data")]
         [SerializeField] private List<ObjectiveSequenceData> _sequenceData;
 
         public string UniqueId => _uniqueId;
 
         public ObjectiveFinishConditionsData FinishConditions => _finishConditions;
+
+        public List<ObjectiveSequenceData> InitializeData => _initializeData;
 
         public List<ObjectiveSequenceData> SequenceData => _sequenceData;
     }
