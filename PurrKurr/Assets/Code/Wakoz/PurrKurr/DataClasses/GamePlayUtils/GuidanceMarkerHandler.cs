c11918710f6048e021fccca80605542a1351b43d
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2025-04-05 18:10:00 +0300

    Bindable Handler for GuidanceMarker on screen

---
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/DataClasses/GamePlayUtils/GuidanceMarkerHandler.cs
@@ -0,0 +1,46 @@
+﻿using Code.Wakoz.PurrKurr.DataClasses.GameCore.CollectableItems;
+using Code.Wakoz.PurrKurr.Screens.Gameplay_Controller;
+using Code.Wakoz.PurrKurr.Screens.Gameplay_Controller.Handlers;
+
+namespace Code.Wakoz.PurrKurr.DataClasses.GamePlayUtils
+{
+    public class GuidanceMarkerHandler : IBindableHandler
+    {
+        private GameplayController _gameEvents;
+        private UiGuidanceController _controller;
+
+        public GuidanceMarkerHandler(GameplayController gameEvents, UiGuidanceController controller) {
+            _gameEvents = gameEvents;
+            _controller = controller;
+        }
+
+        public void Bind() {
+
+            if (_gameEvents == null)
+                return;
+
+            _gameEvents.OnHideGuidanceMarker += HideMarker;
+            _gameEvents.OnShowGuidanceMarker += ShowMarker;
+        }
+
+        public void Unbind() {
+
+            if (_gameEvents == null)
+                return;
+
+            _gameEvents.OnHideGuidanceMarker -= HideMarker;
+            _gameEvents.OnShowGuidanceMarker -= ShowMarker;
+        }
+
+        public void Dispose() {
+
+            _gameEvents = null;
+        }
+
+        private void HideMarker() 
+            => _controller.DeactivateMarker();
+
+        private void ShowMarker(ObjectiveMarker marker) 
+            => _controller.ActivateMarker(marker);
+    }
+}
