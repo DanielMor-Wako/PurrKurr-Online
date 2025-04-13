5737c725d2c4717e3264dabd5b0937e9a76c1ef0
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2025-04-14 01:47:30 +0300

    data class for future player input buffer

---
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/Screens/UI Controller/ExecutableAction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static Code.Wakoz.PurrKurr.DataClasses.Enums.Definitions;
+
+namespace Code.Wakoz.PurrKurr.Screens.Ui_Controller
+{
+    [Serializable]
+    public class ExecutableAction
+    {
+        public ActionInput NavigationInput ;// { get; private set; }
+        public ActionInput ActionInput ;// { get; private set; }
+        public AttackAbility Variant { get; private set; }
+
+        public ExecutableAction(ActionInput navigationInput = null, ActionInput actionInput = null, AttackAbility variant = AttackAbility.None) {
+            
+            NavigationInput = navigationInput;
+            ActionInput = actionInput;
+            Variant = variant;
+        }
+
+        public void SetVariant(AttackAbility newVariant) {
+        
+            // Allow modification for state-based adjustments
+            Variant = newVariant;
+        }
+    }
+}
