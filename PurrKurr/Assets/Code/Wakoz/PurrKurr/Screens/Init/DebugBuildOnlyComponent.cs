8b63c7e7d53d8469826b2b257e55553165501b00
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2025-05-20 04:19:24 +0300

    Component to be removed during build

---
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/Screens/Init/DebugBuildOnlyComponent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Code.Wakoz.PurrKurr.Screens.Init
+{
+    public class DebugBuildOnlyComponent : MonoBehaviour
+    {
+        [Tooltip("Destroy or hide any gameobject while build is Debug or Development")]
+        [SerializeField] private bool _hideAndNotDestroy = false;
+
+        private void Start() {
+
+            if (Debug.isDebugBuild) return;
+
+            if (_hideAndNotDestroy) {
+                gameObject.SetActive(false);
+
+            } else {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
