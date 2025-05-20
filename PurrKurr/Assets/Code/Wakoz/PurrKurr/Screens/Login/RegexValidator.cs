03f45836725525f2a0121a45dbbd66dfd1c0ab20
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2025-05-20 04:28:41 +0300

    Regex validator

---
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/Screens/Login/RegexValidator.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Code.Wakoz.PurrKurr.Screens.Login
+{
+    public sealed class RegexValidator
+    {
+        public bool IsValidEmail(string emailAddress) {
+            if (string.IsNullOrWhiteSpace(emailAddress)) {
+                return false;
+            }
+
+            var emailPattern = @"[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$";
+            
+            return Regex.IsMatch(emailAddress, emailPattern);
+        }
+    }
+}
