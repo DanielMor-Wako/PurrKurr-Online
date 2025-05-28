50ba77b75f11db0a382a4dd9208aeb653cc3f6e1
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2025-05-29 01:33:09 +0300

    Main menu progress bar and buttons state

--- PurrKurr/Assets/Code/Wakoz/PurrKurr/Screens/MainMenu/MainMenuView.cs
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/Screens/MainMenu/MainMenuView.cs
@@ -1,5 +1,6 @@
 ﻿using Code.Wakoz.PurrKurr.Views;
 using System;
+using TMPro;
 using UnityEngine;
 
 namespace Code.Wakoz.PurrKurr.Screens.MainMenu
@@ -7,6 +8,11 @@ namespace Code.Wakoz.PurrKurr.Screens.MainMenu
     public class MainMenuView : View<MainMenuModel>
     {
         [SerializeField] CanvasGroupFaderView _fader;
+        [SerializeField] private TMP_Text _playerNameText;
+
+        [SerializeField] private MultiStateView _menuAvailableState;
+        [SerializeField] private ImageFillerView _loadingProgressBar;
+        [SerializeField] private TMP_Text _loadingProgressText;
 
         public event Action OnAccountClicked;
         public event Action OnPlayCampaignClicked;
@@ -20,9 +26,48 @@ namespace Code.Wakoz.PurrKurr.Screens.MainMenu
         public void ClickedPlayArena() => OnPlayArenaClicked?.Invoke();
         public void ClickedPlayHunt() => OnPlayHuntClicked?.Invoke();
 
+        /// <summary>
+        /// Update progress of text and image filler by the targetValue,
+        /// value must be between 0 to 1, progress is visible when value is above 0
+        /// </summary>
+        /// <param name="targetValue"></param>
+        public void UpdateLoadingBarProgress(float targetValue, string progressText = "") {
+
+            _loadingProgressBar.gameObject.SetActive(targetValue > 0);
+
+            if (targetValue <= 0)
+                return;
+
+            _loadingProgressBar?.StartTransition(targetValue);
+            _loadingProgressText?.SetText(progressText);
+        }
+
+        protected override void ModelReplaced() {
+
+            if (_loadingProgressBar == null) return;
+            _loadingProgressBar.ImageTarget.fillAmount = 0;
+        }
+
         protected override void ModelChanged() {
 
             _fader?.EndTransition(Convert.ToInt32(Model.IsOpen));
+            UpdateDisplayName();
+            UpdateButtonsAvailabeState();
+        }
+
+        private void UpdateDisplayName() {
+
+            if (_playerNameText == null)
+                return;
+
+            _playerNameText?.SetText(Model.PlayerDisplayName);
+        }
+
+        private void UpdateButtonsAvailabeState() {
+
+            if (_menuAvailableState == null) return;
+
+            _menuAvailableState.ChangeState(Convert.ToInt32(Model.AreButtonsAvailable));
         }
 
     }
