4931c31711684224e7d06b4fe5eb869c3cbbc1a1
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2025-05-29 01:35:55 +0300

    todo note

--- PurrKurr/Assets/Code/Wakoz/PurrKurr/DataClasses/GameCore/OverlayWindowTrigger/OverlayWindowConditionData.cs
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/DataClasses/GameCore/OverlayWindowTrigger/OverlayWindowConditionData.cs
@@ -1,18 +1,23 @@
-﻿using Code.Wakoz.PurrKurr.DataClasses.GameCore.ConditionChecker;
-using Code.Wakoz.PurrKurr.UI.Instructions;
+﻿using Code.Wakoz.PurrKurr.UI.Instructions;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Code.Wakoz.PurrKurr.DataClasses.GameCore.OverlayWindowTrigger {
+
     [Serializable]
     public class OverlayWindowConditionData {
+
         [Tooltip("Page index to apply the modifiers")]
         public int PageIndex;
+
+        // Todo: use [SerializedReference] of CharacterConditionChecker instead of component
         [Tooltip("Conditional component to check specific about a character for and then perform an Action when conditions are met")]
         public Component CharacterConditionChecker;
+
         [Tooltip("Apply Slow Motion during")]
         public bool ApplySlowMotion = false;
+
         [Tooltip("Animated Action to display over the UI-Inputs")]
         public List<OverlayWindowAnimationData> AnimatedAction;
     }
