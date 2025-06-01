7d5b52c90b6c5b3f031fb12ed59be744230bd5e8
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2025-06-01 23:17:07 +0300

    Hide marker during active mission/ time challenge

--- PurrKurr/Assets/Code/Wakoz/PurrKurr/DataClasses/GamePlayUtils/GuidanceMarkerHandler.cs
+++ PurrKurr/Assets/Code/Wakoz/PurrKurr/DataClasses/GamePlayUtils/GuidanceMarkerHandler.cs
@@ -1,7 +1,9 @@
-﻿using Code.Core;
-using Code.Wakoz.PurrKurr.DataClasses.GameCore.CollectableItems;
+﻿using Code.Wakoz.PurrKurr.DataClasses.GameCore.CollectableItems;
+using Code.Wakoz.PurrKurr.DataClasses.Objectives;
+using Code.Wakoz.PurrKurr.DataClasses.ScriptableObjectData;
 using Code.Wakoz.PurrKurr.Screens.Gameplay_Controller;
 using Code.Wakoz.PurrKurr.Screens.Gameplay_Controller.Handlers;
+using UnityEngine;
 
 namespace Code.Wakoz.PurrKurr.DataClasses.GamePlayUtils
 {
@@ -9,6 +11,9 @@ namespace Code.Wakoz.PurrKurr.DataClasses.GamePlayUtils
     {
         private GameplayController _gameEvents;
         private UiGuidanceController _controller;
+
+        private bool _forceHideMarker;
+        private ObjectiveMarker _currentMarker;
 
         public GuidanceMarkerHandler(GameplayController gameEvents, UiGuidanceController controller) {
             _gameEvents = gameEvents;
@@ -22,6 +27,10 @@ namespace Code.Wakoz.PurrKurr.DataClasses.GamePlayUtils
 
             _gameEvents.OnHideGuidanceMarker += HideMarker;
             _gameEvents.OnShowGuidanceMarker += ShowMarker;
+
+            _gameEvents.OnObjectiveMissionStarted += StartForceHiding;
+            _gameEvents.OnHeroReposition += StopForceHiding;
+            _gameEvents.OnHeroDeath += TagNoneActiveMission;
         }
 
         public void Unbind() {
@@ -31,6 +40,10 @@ namespace Code.Wakoz.PurrKurr.DataClasses.GamePlayUtils
 
             _gameEvents.OnHideGuidanceMarker -= HideMarker;
             _gameEvents.OnShowGuidanceMarker -= ShowMarker;
+
+            _gameEvents.OnObjectiveMissionStarted -= StartForceHiding;
+            _gameEvents.OnHeroReposition -= StopForceHiding;
+            _gameEvents.OnHeroDeath -= TagNoneActiveMission;
         }
 
         public void Dispose() {
@@ -38,10 +51,45 @@ namespace Code.Wakoz.PurrKurr.DataClasses.GamePlayUtils
             _gameEvents = null;
         }
 
-        private void HideMarker() 
-            => _controller.DeactivateMarker();
+        private void HideMarker() {
+            
+            _currentMarker = null;
+            _controller.DeactivateMarker();
+        }
 
-        private void ShowMarker(ObjectiveMarker marker) 
-            => _controller.ActivateMarker(marker);
+        private void ShowMarker(ObjectiveMarker marker) {
+            
+            _currentMarker = marker;
+
+            if (_forceHideMarker) {
+                _controller.DeactivateMarker();
+                return;
+            }
+            
+            _controller.ActivateMarker(marker);
+        }
+
+        private void ForceHideMarker(bool isHiding) {
+
+            _forceHideMarker = isHiding;
+
+            if (_forceHideMarker || _currentMarker == null) {
+            
+                _controller.DeactivateMarker();
+                return;
+            }
+            
+            _controller.ActivateMarker(_currentMarker);
+        }
+
+        private void StartForceHiding(IObjective objective, ObjectiveSequenceDataSO sO)
+            => ForceHideMarker(true);
+
+        private void StopForceHiding(Vector3 vector) 
+            => ForceHideMarker(false);
+
+        private void TagNoneActiveMission() 
+            => ForceHideMarker(false);
+
     }
 }
