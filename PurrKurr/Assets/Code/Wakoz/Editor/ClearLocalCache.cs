7d57d22eb1513800b947ae7a3eede4069b5c1312
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2025-06-03 01:50:33 +0300

    Editor tool to remove cached data

---
+++ PurrKurr/Assets/Code/Wakoz/Editor/ClearLocalCache.cs
@@ -0,0 +1,32 @@
+﻿using Code.Core;
+using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Code.Wakoz.Editor
+{
+    public static class ClearLocalCache
+    {
+        [MenuItem("Wakoz/Clear Local Cache", priority = 1202)]
+        public static async void DeleteLocalCache() {
+
+            string header = $"Delete {CacheConfig.CacheFilePath}?";
+            string leftButton = "Nah";
+            string RightButton = "Yes";
+
+            try {
+                await EditorCancelOkPopup.Open(header, leftButton, RightButton);
+                var filePath = Path.Combine(Application.persistentDataPath, CacheConfig.CacheFilePath);
+                if (File.Exists(filePath)) {
+                    File.Delete(filePath);
+                }
+                Debug.Log("Cleared Cache file");
+            }
+            catch (OperationCanceledException) {
+                Debug.Log("Keeping Cache file");
+            }
+        }
+    } 
+}
+
